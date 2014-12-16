4b00143aa9860f8cd427029980ab29028ba1f5eb
Michel Roca <mroca.dh@gmail.com>
2014-12-16 20:09:08 +0100

    Add global add button

--- app/layout/layout.js
+++ app/layout/layout.js
@@ -1,7 +1,33 @@
 'use strict';
 
 angular.module('myApp.layout', ['ngMaterial'])
+    .factory('layoutService', function () {
+        var layoutService = {};
 
+        var addButtonAction = null;
+        var addButtonScopesWatcher = [];
+
+        layoutService.addButton = {
+            setAction: function (callback) {
+                addButtonAction = callback;
+                this.emit();
+            },
+            clear: function () {
+                addButtonAction = null;
+                this.emit();
+            },
+            subscribe: function (scope, localVar) {
+                addButtonScopesWatcher.push([scope, localVar || 'addButtonAction']);
+            },
+            emit: function () {
+                angular.forEach(addButtonScopesWatcher, function (scope) {
+                    scope[0][scope[1]] = addButtonAction;
+                });
+            }
+        };
+
+        return layoutService;
+    })
     .controller('ToolbarCtrl', ['$scope', '$mdSidenav', function ($scope, $mdSidenav) {
         $scope.toggleLeft = function () {
             $mdSidenav('left').toggle();
@@ -11,5 +37,9 @@ angular.module('myApp.layout', ['ngMaterial'])
         $scope.close = function () {
             $mdSidenav('left').close();
         };
+    }])
+    .controller('AddButton', ['$scope', 'layoutService', function ($scope, layoutService) {
+        $scope.addButtonAction = null;
+        layoutService.addButton.subscribe($scope);
     }])
 ;
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.view1', ['ngRoute', 'myApp.layout'])
 
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/view1', {
@@ -9,29 +9,8 @@ angular.module('myApp.view1', ['ngRoute'])
         });
     }])
 
-    .controller('View1Ctrl', ['$scope', '$mdDialog', function ($scope, $mdDialog) {
+    .controller('View1Ctrl', ['$scope', '$mdDialog', 'layoutService', function ($scope, $mdDialog, layoutService) {
         $scope.todos = [
-            {
-                face: 'HTTP',
-                what: 'Brunch this weekend?',
-                who: 'Min Li Chan',
-                when: '3:08PM',
-                notes: " I'll be in your neighborhood doing errands"
-            },
-            {
-                face: 'HTTP',
-                what: 'Brunch this weekend?',
-                who: 'Min Li Chan',
-                when: '3:08PM',
-                notes: " I'll be in your neighborhood doing errands"
-            },
-            {
-                face: 'HTTP',
-                what: 'Brunch this weekend?',
-                who: 'Min Li Chan',
-                when: '3:08PM',
-                notes: " I'll be in your neighborhood doing errands"
-            },
             {
                 face: 'HTTP',
                 what: 'Brunch this weekend?',
@@ -48,38 +27,46 @@ angular.module('myApp.view1', ['ngRoute'])
             }
         ];
 
-        $scope.showTodo = function (e, item) {
-            console.log(item);
+        $scope.selectedIndex = null;
+
+        $scope.showTodo = function (e, index, item) {
+            $scope.selectedIndex = index;
 
             $mdDialog.show({
                 controller: DialogController,
                 templateUrl: 'view1/item.dialog.html',
                 targetEvent: e,
                 locals: {item: item}
-                //bindToController: true
+                //bindToController: false
             })
-                .then(function (answer) {
-                    $scope.alert = 'You said the information was "' + answer + '".';
-                    console.log($scope.alert);
+                .then(function (updatedItem) {
+                    if (item) {
+                        angular.extend(item, updatedItem);
+                    } else {
+                        $scope.todos.push(updatedItem);
+                    }
 
+                    $scope.selectedIndex = null;
                 }, function () {
-                    $scope.alert = 'You cancelled the dialog.';
-                    console.log($scope.alert );
+                    $scope.selectedIndex = null;
                 });
         };
+
+        layoutService.addButton.setAction($scope.showTodo);
+
+        $scope.$on("$destroy", function () {
+            layoutService.addButton.clear();
+        });
     }]);
 
 function DialogController($scope, $mdDialog, item) {
+    $scope.item = item ? angular.copy(item) : {};
 
-    $scope.item = item;
-
-    $scope.hide = function () {
-        $mdDialog.hide();
+    $scope.save = function () {
+        $mdDialog.hide($scope.item);
     };
+
     $scope.cancel = function () {
         $mdDialog.cancel();
-    };
-    $scope.answer = function (answer) {
-        $mdDialog.hide(answer);
     };
 }
